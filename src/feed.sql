856e811c89a48a1ace0646581acf7938dd717a56
Zachari <zacharigervais04@gmail.com>
2023-05-18 14:03:29 -0400

    SQL 14h03

--- src/feed.sql
+++ src/feed.sql
@@ -1,21 +1,4 @@
 -- Insertions Table Campus --
-INSERT INTO Campus (campus_id, nom) VALUES (1,'Campus de Longueuil');
-INSERT INTO Campus (campus_id, nom) VALUES (2,'Campus de l''Ouest');
-INSERT INTO Campus (campus_id, nom) VALUES (3,'Campus de l''Est');
-
-
--- Insertions Table Pavillon --
-INSERT INTO Pavillon (pavillon_id, nom, campus_id) VALUES ('C1', 'J.-Armand-Bombardier', 2);
-INSERT INTO Pavillon (pavillon_id, nom, campus_id) VALUES ('D7', 'Marie-Victorin', 2);
-
---Insertions Table Departement --
-INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('elec','Département de génie électrique et de génie informatique', 'C1');
-INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('mec','Département de génie mécanique', 'C1');
-INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('facile','Département de génie civil et de génie du bâtiment', 'C1');
-INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('info','Département d''informatique', 'D7');
---Insertion personne--
-INSERT INTO personne(nom, prénom, cip, departement_id) VALUES ('aubry', 'olivier', 'aubo1502','info')
---Insertion fonction--
---Insertion local--
-
-INSERT INTO local(local_id, capacite, notes, pavillon_id, fonction_id ) VALUES (1007,21,'grand','C1')
+INSERT INTO Campus (campus_id, nom) VALUES (1, 'Campus de Longueuil');
+INSERT INTO Campus (campus_id, nom) VALUES (2, 'Campus de l''Ouest');
+INSERT INTO Campus (campus_id, nom) VALUES (3, 'Campus de l''Est');
--- src/init.sql
+++ src/init.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Campus
 (
     campus_ID INT NOT NULL,
@@ -8,20 +9,19 @@ CREATE TABLE Campus
 CREATE TABLE Caracteristique
 (
     caracteristique_id INT NOT NULL,
-    nom INT NOT NULL,
+    nom VARCHAR NOT NULL,
     PRIMARY KEY (caracteristique_id)
 );
 
 CREATE TABLE Statut
 (
-    nom VARCHAR NOT NULL,
-    statut_id INT NOT NULL,
-    PRIMARY KEY (statut_id)
+    nom_statut VARCHAR NOT NULL,
+    PRIMARY KEY (nom_statut)
 );
 
 CREATE TABLE Fonction
 (
-    nom INT NOT NULL,
+    nom VARCHAR NOT NULL,
     fonction_id INT NOT NULL,
     PRIMARY KEY (fonction_id)
 );
@@ -31,7 +31,7 @@ CREATE TABLE Pavillon
     pavillon_id VARCHAR NOT NULL,
     nom VARCHAR NOT NULL,
     campus_ID INT NOT NULL,
-    PRIMARY KEY (pavillon_id),
+    PRIMARY KEY (pavillion_id),
     FOREIGN KEY (campus_ID) REFERENCES Campus(campus_ID)
 );
 
@@ -58,16 +58,17 @@ CREATE TABLE Local
 
 CREATE TABLE Sous_local
 (
-    note INT,
+    note VARCHAR,
     capacite INT NOT NULL,
-    sous_local_id INT NOT NULL,
+    sous_local_id VARCHAR NOT NULL,
     local_id INT NOT NULL,
     pavillon_id VARCHAR NOT NULL,
     FOREIGN KEY (local_id, pavillon_id) REFERENCES Local(local_id, pavillon_id)
     );
 
-CREATE TABLE possede
+CREATE TABLE local_caracteristique
 (
+    quantite INT NOT NULL,
     local_id INT NOT NULL,
     pavillon_id VARCHAR NOT NULL,
     caracteristique_id INT NOT NULL,
@@ -89,9 +90,9 @@ CREATE TABLE Personne
 CREATE TABLE Reservation
 (
     reservation_id INT NOT NULL,
-    date_debut INT NOT NULL,
-    date_fin INT NOT NULL,
-    description INT,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    description VARCHAR,
     cip VARCHAR NOT NULL,
     local_id INT NOT NULL,
     pavillon_id VARCHAR NOT NULL,
@@ -100,11 +101,21 @@ CREATE TABLE Reservation
     FOREIGN KEY (local_id, pavillon_id) REFERENCES Local(local_id, pavillon_id)
 );
 
-CREATE TABLE associer
+CREATE TABLE Trigger
+(
+    trigger_id INT NOT NULL,
+    description VARCHAR NOT NULL,
+    trigger_date DATE NOT NULL,
+    reservation_id INT NOT NULL,
+    PRIMARY KEY (trigger_id),
+    FOREIGN KEY (reservation_id) REFERENCES Reservation(reservation_id)
+);
+
+CREATE TABLE personne_statut
 (
     cip VARCHAR NOT NULL,
-    statut_id INT NOT NULL,
-    PRIMARY KEY (cip, statut_id),
+    nom_statut VARCHAR NOT NULL,
+    PRIMARY KEY (cip, nom_statut),
     FOREIGN KEY (cip) REFERENCES Personne(cip),
-    FOREIGN KEY (statut_id) REFERENCES Statut(statut_id)
+    FOREIGN KEY (nom_statut) REFERENCES Statut(nom_statut)
 );
