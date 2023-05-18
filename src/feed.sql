c78fda396ae0fc35235f73ffaefbe41507a5b797
olivier aubr <aubo1502@usherbrooke.ca>
2023-05-18 11:13:50 -0400

    jsp

--- src/feed.sql
+++ src/feed.sql
@@ -9,7 +9,13 @@ INSERT INTO Pavillon (pavillon_id, nom, campus_id) VALUES ('C1', 'J.-Armand-Bomb
 INSERT INTO Pavillon (pavillon_id, nom, campus_id) VALUES ('D7', 'Marie-Victorin', 2);
 
 --Insertions Table Departement --
-INSERT INTO Departement (nom, pavillon_id) VALUES ('Département de génie électrique et de génie informatique', 'C1');
-INSERT INTO Departement (nom, pavillon_id) VALUES ('Département de génie mécanique', 'C1');
-INSERT INTO Departement (nom, pavillon_id) VALUES ('Département de génie civil et de génie du bâtiment', 'C1');
-INSERT INTO Departement (nom, pavillon_id) VALUES ('Département d''informatique', 'D7');
+INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('elec','Département de génie électrique et de génie informatique', 'C1');
+INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('mec','Département de génie mécanique', 'C1');
+INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('facile','Département de génie civil et de génie du bâtiment', 'C1');
+INSERT INTO Departement (departement_id,nom, pavillon_id) VALUES ('info','Département d''informatique', 'D7');
+--Insertion personne--
+INSERT INTO personne(nom, prénom, cip, departement_id) VALUES ('aubry', 'olivier', 'aubo1502','info')
+--Insertion fonction--
+--Insertion local--
+
+INSERT INTO local(local_id, capacite, notes, pavillon_id, fonction_id ) VALUES (1007,21,'grand','C1')
