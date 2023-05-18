8acefa5295fad6b66d7d4672408ab0dd971f06f9
Zachari <zacharigervais04@gmail.com>
2023-05-18 14:03:29 -0400

    SQL 14h03

--- src/feed.sql
+++ src/feed.sql
@@ -1,4 +1,4 @@
 -- Insertions Table Campus --
-INSERT INTO Campus (campus_id, nom) VALUES ('Campus de Longueuil', 'Adresse Longueuil');
-INSERT INTO Campus (campus_id, nom) VALUES ('Campus de l''Ouest', 'Adresse Campus principale');
-INSERT INTO Campus (campus_id, nom) VALUES ('Campus de l''Est', 'Adresse Campus de la santé');
+INSERT INTO Campus (campus_id, nom) VALUES (1, 'Campus de Longueuil');
+INSERT INTO Campus (campus_id, nom) VALUES (2, 'Campus de l''Ouest');
+INSERT INTO Campus (campus_id, nom) VALUES (3, 'Campus de l''Est');
