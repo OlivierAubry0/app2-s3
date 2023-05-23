2c253df18468fd62413b2023ea5dbe897668967a
Zachari <zacharigervais04@gmail.com>
2023-05-23 12:24:01 -0400

    avant valide

--- src/requete.sql
+++ src/requete.sql
@@ -40,6 +40,7 @@ ORDER BY
 $$;
 
 SELECT * FROM TABLEAU('2023-05-21 00:00:00'::timestamp, '2023-05-21 23:45:00'::timestamp, 0110);
+
 CREATE OR REPLACE FUNCTION update_logbook() RETURNS TRIGGER AS $$
 BEGIN
     IF TG_OP = 'INSERT' THEN
@@ -50,16 +51,16 @@ BEGIN
             WHERE local_id = NEW.local_id
               AND ((cubicule_id = NEW.cubicule_id) OR (cubicule_id IS NULL AND NEW.cubicule_id IS NULL))
               AND pavillon_id = NEW.pavillon_id
-              AND date_debut < NEW.date_fin
-              AND date_fin > NEW.date_debut
+              AND date_debut < date_fin
+              AND date_fin > date_debut
               AND reservation_id <> NEW.reservation_id
         ) THEN
             RAISE EXCEPTION 'Reservation conflict: The selected time interval overlaps with an existing reservation.';
         END IF;
 
         -- Insert the reservation into the logbook
-        INSERT INTO logbook (trigger_id, description, trigger_date, reservation_id, cip)
-        VALUES (DEFAULT, 'Reservation created', CURRENT_DATE, NEW.reservation_id, NEW.cip);
+        INSERT INTO logbook (trigger_id, description, trigger_date, local_id, pavillon_id, cubicule_id, cip)
+        VALUES (DEFAULT, 'Reservation created', CURRENT_DATE, NEW.local_id, NEW.pavillon_id, NEW.cubicule_id, NEW.cip);
 
         RETURN NEW;
     ELSIF TG_OP = 'UPDATE' THEN
@@ -79,8 +80,9 @@ BEGIN
         -- Update the reservation in the logbook
         UPDATE logbook
         SET description = 'Reservation updated', trigger_date = CURRENT_DATE
-        WHERE reservation_id = NEW.reservation_id;
-
+        WHERE local_id = NEW.local_id
+        AND pavillon_id = NEW.pavillon_id
+        AND cubicule_id = NEW.cubicule_id;
         RETURN NEW;
     END IF;
 END;
@@ -96,8 +98,8 @@ EXECUTE FUNCTION update_logbook();
 CREATE OR REPLACE FUNCTION procedure_reservation_delete()
     RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO logbook(trigger_id,description, trigger_date, cip, reservation_id)
-    VALUES(DEFAULT,'Reservation annule', CURRENT_DATE, OLD.cip, OLD.reservation_id);
+    INSERT INTO logbook(trigger_id,description, trigger_date,local_id,pavillon_id,cubicule_id, cip)
+    VALUES(DEFAULT,'Reservation annule', CURRENT_DATE, OLD.local_id, OLD.pavillon_id, OLD.cubicule_id, OLD.cip);
     RETURN old;
 END;
 
